30b2225d6c3f4e131d227296af87fdce6b63c219
unknown <iveejnas45@gmail.com>
2021-02-03 20:48:24 +0530

    it contains about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,15 @@
-import leaf from '../image/leaf.jpg';
+import LeafImage from '../image/leaf.jpg';
+import Button from './Button';
+import ColMd4 from './ColMd4';
+import Row from './Row';
 
 function About(){
     return(
-        <div className="row my-3" >
-          <div className="col-1"></div>
-          <div className="col-5">
+        <Row className="my-3 mt-5" >
+          <div className="col-md-1"></div>
+          <div className="col-md-5">
             <h4 className="text-success">About Us</h4>
+            <h2 className="text-bold">History About Herballife</h2>
             <p>
               Herbalife Nutrition is a global nutrition company whose purpose is to make the world
               healthier and happier. The Company has been on a mission for nutrition - changing people's
@@ -16,12 +20,13 @@ function About(){
               some of which have been linked to negative health effects. Short-term use likely causes
               weight loss, but long-term effectiveness is yet to be studied.
                   </p>
+                  <Button className="bg-white border-green text-success" name="View More"/>
 
           </div>
-          <div className="col-4">
-            <img src={leaf} height="100%" width="100%" />
-          </div>
-        </div>
+          <ColMd4>
+            <img src={LeafImage} height="100%" width="100%" />
+          </ColMd4>
+        </Row>
     )
 }
 
