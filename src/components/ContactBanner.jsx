2cf63e13f8b94f2f6445a776b1c88f16006f23e4
unknown <iveejnas45@gmail.com>
2021-02-04 10:50:32 +0530

    this component contains banner .inside the banner contanints contact information

--- src/components/ContactBanner.jsx
+++ src/components/ContactBanner.jsx
@@ -1,16 +1,19 @@
-import ContactBanner1 from "../image/contact-banner.jpg";
+import BackgroundImage from "../image/contact-banner.jpg";
+import Button from "./Button";
+import ColMd4 from "./ColMd4";
+import Row from "./Row";
 
 function ContactBanner(){
     return(
         <div>
-        <img src={ContactBanner1} className="banner-background" height="50%" width="100%" />
-        <div className="row contact-banner-detail"  >
-          <div className="col-4"></div>
-          <div align="center" className="col-4">
+        <img src={BackgroundImage} className="banner-background" height="50%" width="98%" />
+        <Row className="contact-banner-detail"  >
+          <ColMd4 />
+          <ColMd4 className="text-center">
             <h2 className="text-white">Contact Us</h2>
-            <button className="button-1">Contact Us</button>
-          </div>
-        </div>
+            <Button className="bg-transparent text-white border-white" name="Contact Us" />
+          </ColMd4>
+        </Row>
       </div>
     )
 }
