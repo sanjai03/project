0f9b084d0c8d702abc888e42738aea7fd66d0982
unknown <iveejnas45@gmail.com>
2021-02-04 10:59:19 +0530

    this component contains for show post by get details from api

--- src/components/LatestPost.jsx
+++ src/components/LatestPost.jsx
@@ -1,26 +1,29 @@
-import NewsCard from './PostCard'
+import PostCard from './PostCard'
 import image from '../image/nutrion.jpg'
+import Row from './Row'
+import Button from './Button'
+import ColMd4 from './ColMd4'
 function LatestPost(props){
     
     return(
-        <div className="row my-3">
+        <Row className=" my-3">
         <div className="col-1"></div>
         <div className ="col-10 mb-3 row">
-        <div className="col-10"><h4 className="text-success">Recent News</h4><h2 className="align-left">Our Latest News</h2></div>
-        <div className="col-2"><button className="button1">view more</button></div>
+        <div className="col-10"><h4 className="text-success">RECENT NEWS</h4><h2 className="align-left">Our Latest News</h2></div>
+        <div className="col-2"><Button className="border-green text-success bg-white" name="View More"/></div>
         {
             props.posts.map((post)=>
                 
-                   ( <div key={post.id} className="col-4 mb-3">
-                        <NewsCard name={post.title} id={post.id} desc={post.body} />
-                    </div>
+                   ( <ColMd4 key={post.id} className=" mb-3">
+                        <PostCard name={post.title} id={post.id} desc={post.body} />
+                    </ColMd4>
                    )
                 
             )
         }
         
         </div>
-</div>
+</Row>
     )
 }
 export default LatestPost
