dc6a8fdcb086386d7b1c506931ec4aba60e4f12a
unknown <iveejnas45@gmail.com>
2021-02-04 10:55:39 +0530

    this component contains header of application

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
-import banner from '../image/banner.jpg';
+import Logo from '../image/banner.jpg';
 import List from "./List";
 function Home(){
     return(
     
         <header className="nav-background">
   <div className="container">
-    <nav className="navbar navbar-expand-sm ">
-      <a href="/"><img src={banner}  height="30px"/></a>
+    <nav className="navbar navbar-expand-lg navbar-dark nav-background ">
+      <a className="navbar-brand" href="/"><img src={Logo}  height="30px"/></a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon" />
+  </button>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="pl-2 navbar-nav ">
-        <List text="Home" />
+        <List text="Home" href="/" />
         <List text="Herbal Life" />
         <List text="Store" />
         <List text="Gallary" />
@@ -22,7 +26,10 @@ function Home(){
         <List icon="instagram" />
         <List icon="youtube" />
       </ul>
+      </div>
+     
     </nav>
+    
   </div>
 </header>
 
