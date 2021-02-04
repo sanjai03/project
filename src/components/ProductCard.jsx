7948f2a415f060caa73322b21b9571b710354417
unknown <iveejnas45@gmail.com>
2021-02-04 11:03:43 +0530

    this component contains for product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,12 @@
+import Button from "./Button"
+
 function ProductCard(props){
     return(
-        <div className="p-3 card">
+        <div className="p-3 mb-4 card">
                     <img src={props.img} width="100%" className="px-1 py-1" />
     <p className=" product-txt px-1">{props.name}</p>
     <p className="product-txt px-1 text-center text-success">{props.rate}</p>
-                    <button   className={ props.btnClr==='red'?"ml-5 text-white bg-danger col-6":'ml-5 text-white bg-success col-6'}>Buy</button>
+                    <Button   className={ props.buttonColour==='red'?"ml-5 text-white bg-danger col-6":'ml-5 text-white bg-success col-6'}>Buy</Button>
                   </div>
     )
 }
