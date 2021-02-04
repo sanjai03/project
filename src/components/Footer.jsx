662eb20ef1bad2b63a7f6cdda13a74fa8b96234b
unknown <iveejnas45@gmail.com>
2021-02-04 10:52:15 +0530

    this component contains footer of application

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
 import logo from '../image/logo.png'
+import AddressContent from './AddressContent';
+import ColMd4 from './ColMd4';
+import Row from './Row';
 function Footer(){
+  let date = new Date();
+  let year = date.getFullYear();
     return(
         <footer className="bg-dark">
-            <div className="footer-block row py-2 my-3 col-9">
-              <div className="col-4">
+            <Row className="footer-block py-2 my-3 col-9">
+              <ColMd4>
                 <div >
                   <img src={logo} alt="logo" className="footer-logo"  />
   
@@ -13,24 +18,19 @@ function Footer(){
                   activities, promote and maintain a lifetime of good health and make your best shape a reality.
                   
                 </p>
-              </div>
-              <div className="col-4">
+              </ColMd4>
+              <ColMd4>
                 <h2 className="text-success">CONTACT US</h2>
-                  <span className="text-white" ><i className=" fa text-success fa-address-card"></i>
-                  68/75-79, Subba Road Avenue, Nungambakkam, Chennai, Tamil Nadu 600034</span><br/>
-                  <span className="text-white" ><i className=" fa text-success fa-phone"></i>
-                    044-353654</span><br/>
-                    <span className="text-white" ><i className=" fa text-success fa-envelope-square"></i>
-                    nutrient@gmail.com</span>
-              </div>
-              <div className="col-4">
+                  <AddressContent />
+                </ColMd4>
+              <ColMd4>
                 <h2 className="text-success">SUBSCRIBE US</h2>
                 <p className="text-white">stay updated with our latest news.we promise not to spam</p>
                 <div className="bg-success text-white p-3 footer-btn">Your Email<i className="float-right fa fa-envelope"></i></div>
-              </div>
+                </ColMd4>
               
-            </div>
-            <div className="text-center text-white footer-bottom"> copyrights 2021 herbal</div>
+            </Row>
+            <div className="text-center text-white footer-bottom"> copyrights {year} herbal</div>
           </footer>
     )
 }
