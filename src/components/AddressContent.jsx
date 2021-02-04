b6dacb6385eaf8d329dad2a0faa706d3caed0676
unknown <iveejnas45@gmail.com>
2021-02-04 10:44:55 +0530

    this component contains Address

---
+++ src/components/AddressContent.jsx
@@ -0,0 +1,16 @@
+import SpanTextWhite from "./SpanTextWhite"
+
+function AddressContent(){
+    return(
+    <>
+        <SpanTextWhite><i className=" fa text-white fa-address-card"></i>
+                        68/75-79, Subba Road Avenue, Nungambakkam, Chennai, Tamil Nadu 600034</SpanTextWhite><br />
+              <SpanTextWhite className="text-white" ><i className=" fa text-white fa-phone"></i>
+                          044-353654</SpanTextWhite><br />
+              <SpanTextWhite className="text-white" ><i className=" fa text-white fa-envelope-square"></i>
+                          nutrient@gmail.com</SpanTextWhite><br/>
+                          </>
+    )
+}
+
+export default AddressContent
